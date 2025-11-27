7bd583668719db7b24263281950e511d9d10f925
lucas <lucasgomes90986@gmail.com>
2025-11-27 17:57:00 -0300

    envio de arquivo

---
+++ app.js
@@ -0,0 +1,112 @@
+// let cliente = 'joao'
+
+// const vendedor = 'joao'
+
+// nsole.log(`O cliente é ${cliente} e o vendedor é ${vendedora}`)
+
+// cliente = 'josé'
+
+// vendedora = 'joana'
+
+//  console.log(`O cliente é ${cliente} e o vendedor é ${vendedora}`)
+
+ //
+/*
+const venda = {
+    cliente: 'joão'
+    vendedor: 'joana'
+}
+
+venda.cliente = 'José'
+
+venda.cliente = 'josé'
+venda.valor = 12000
+
+venda.produto ='iphone 17 pro Max'
+ 
+const paciente ={
+
+    
+    nome = "José" , peso : 68, altura: 1.65, imc:"",
+}
+
+const paciente = {
+    nome: document,qu
+    peso: 68,
+    altura: 1.65,
+    imc:"",
+};
+
+
+paciente.imc = ""
+paciente.peso = 68
+paciente.altura = 1.65
+paciente.nome ="josé"
+
+
+
+
+console.log(venda.produto.toUppercase())
+console.log(venda.valor.tolocaleString('pt-br' , {style: 'currency', currency: 'BRL'}))
+
+//console.log(´O paciente ${nome} tem um IMC de $ {imc.toFixed(1)}`)
+
+if(imc < 18.5){
+    console.log(`Paciente abaixo do peso. IMC é de: ${imc}`)
+}else if (paciente imc >= 18.5 && imc <=24.9){
+    console.log(`Paciente tem peso normal. IMC é de: ${imc} `)
+}else if (paciente imc >= 24.9 && imc < 29.9){
+    console.log(`Paciente tem exesso de peso. IMC é de: ${imc}`)
+}else if (pacienteimc >= 29.9 && imc < 34.9){
+    console.log(`Paciente tem obecidade classe 1. IMC é de: ${imc}`)
+}else if (paciente imc >= 34.9 && imc <= 39.9){
+    console.log(`Paciente tem obessidade classe 2. IMC é de: ${imc}`)
+}else{
+    console.log(`Pacienete tem obesidade classe 3. IMC é de: ${imc}` )
+}
+
+/*
+const titulo = document.querySelector('h1')
+
+const body = document.querySelector('body')
+
+console.log(titulo)
+
+titulo.innerText = 'Curso de Desenvolvimento Jvascript'
+
+const novoParagrafo = document.createElement('h2')
+
+novooParagrafo.innerText = 'Novo paragrafo criado do JS'
+
+body.append (novoParagrafo)
+
+novoParagrafo.style.color ='red'*/
+
+/*
+const nome = document.querySelector('#nome')
+console.log(nome)
+
+const sobrenome = document.querySelector('#sobrenome')
+console.log(sobrenome)
+
+const altura = document.querySelector('#altura')
+console.log(altura)
+
+const peso = document.querySelector('#nome')
+console.log(peso)
+
+const botao = document.querySelector('button')
+const form = document.querySelector('form')
+
+console.log(botao, form)
+
+function teste(){
+    form.addEventListener('submit',()=>{
+        evento.prevenDeFault()
+        window.alert('Evento Recebido')
+    })
+
+}
+
+teste()
+*/                              
