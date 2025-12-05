22127d8185e5cb821901baf1e12a6ffba12490f0
lucas <lcasgomes90986@gmail.com>
2025-12-05 15:59:57 -0300

    envio calculadora consumo

--- app.js
+++ app.js
@@ -1,112 +1,126 @@
-// let cliente = 'joao'
-
-// const vendedor = 'joao'
-
-// nsole.log(`O cliente é ${cliente} e o vendedor é ${vendedora}`)
-
-// cliente = 'josé'
-
-// vendedora = 'joana'
-
-//  console.log(`O cliente é ${cliente} e o vendedor é ${vendedora}`)
-
- //
 /*
-const venda = {
-    cliente: 'joão'
-    vendedor: 'joana'
-}
+CRIAR UMA CALCULADORA DE IMC, ONDE O USUÁRIO DEVE PREENCHER SEU
+PESO, ALTURA E NOME.
 
-venda.cliente = 'José'
+DEVERÁ MOSTRAR NO CONSOLE.LOG APENAS O IMC DO USUÁRIO
 
-venda.cliente = 'josé'
-venda.valor = 12000
 
-venda.produto ='iphone 17 pro Max'
- 
-const paciente ={
+/////////////////// Exemplo de manipulação do DOM ///////////////////
 
-    
-    nome = "José" , peso : 68, altura: 1.65, imc:"",
-}
+// const titulo = document.querySelector('h1')
 
-const paciente = {
-    nome: document,qu
-    peso: 68,
-    altura: 1.65,
-    imc:"",
+// const body = document.querySelector('body')
+
+// console.log(titulo)
+
+// titulo.innerText = 'Curso de Desenvolvimento JavaScript'
+
+// const novoParagrafo = document.createElement('h2')
+
+// novoParagrafo.innerText = 'Novo paragrafo criado no JS'
+
+// body.append(novoParagrafo)
+
+// novoParagrafo.style.color = 'red'
+
+*/
+
+// Criação de objeto paciente para armazenar os dados do usuário
+const inputPaciente = {
+  nome: document.querySelector("#nome"),
+  sobrenome: document.querySelector("#sobrenome"),
+  peso: document.querySelector("#peso"),
+  altura: document.querySelector("#altura"),
 };
 
+const dados = {
+  nome,
+  sobrenome,
+  peso,
+  altura,
+  imc: "",
+};
 
-paciente.imc = ""
-paciente.peso = 68
-paciente.altura = 1.65
-paciente.nome ="josé"
+// Seleção dos elementos do DOM / HTML
+const botao = document.querySelector("button");
+const form = document.querySelector("form");
 
+// paciente.imc = paciente.peso / paciente.altura ** 2;
 
+// Função que dispara um alerta ao submeter / enviar o formulário
+function capturarDados() {
+  form.addEventListener("submit", (evento) => {
+    // Evita o comportamento padrão do formulário de recarregar a página
+    evento.preventDefault();
 
+    /* Recebe os valores (.value) dos inputs de entrada do formulário 
+      e salva dentro do objeto dados (const dados - ver linha 36 )
+    */
+    dados.nome = inputPaciente.nome.value;
+    dados.sobrenome = inputPaciente.sobrenome.value;
+    dados.peso = Number(inputPaciente.peso.value);
+    dados.altura = Number(inputPaciente.altura.value);
 
-console.log(venda.produto.toUppercase())
-console.log(venda.valor.tolocaleString('pt-br' , {style: 'currency', currency: 'BRL'}))
-
-//console.log(´O paciente ${nome} tem um IMC de $ {imc.toFixed(1)}`)
-
-if(imc < 18.5){
-    console.log(`Paciente abaixo do peso. IMC é de: ${imc}`)
-}else if (paciente imc >= 18.5 && imc <=24.9){
-    console.log(`Paciente tem peso normal. IMC é de: ${imc} `)
-}else if (paciente imc >= 24.9 && imc < 29.9){
-    console.log(`Paciente tem exesso de peso. IMC é de: ${imc}`)
-}else if (pacienteimc >= 29.9 && imc < 34.9){
-    console.log(`Paciente tem obecidade classe 1. IMC é de: ${imc}`)
-}else if (paciente imc >= 34.9 && imc <= 39.9){
-    console.log(`Paciente tem obessidade classe 2. IMC é de: ${imc}`)
-}else{
-    console.log(`Pacienete tem obesidade classe 3. IMC é de: ${imc}` )
+    /* Dispara a função que irá calcular o IMC e envia dentro dela o 
+   objeto const dados que contém todos os valores dos inputs digitados.
+  */
+    calcularIMC(dados);
+  });
 }
 
 /*
-const titulo = document.querySelector('h1')
+Função para calcular o IMC. 
+Ela recebe os valores de cálculo dentro do parenteses (entradaDados)
+*/
+function calcularIMC(entradaDados) {
+  // Pega o valor da divisão de peso pelo quadrado da altura e salva em dados.imc
+  dados.imc = entradaDados.peso / entradaDados.altura ** 2;
 
-const body = document.querySelector('body')
-
-console.log(titulo)
-
-titulo.innerText = 'Curso de Desenvolvimento Jvascript'
-
-const novoParagrafo = document.createElement('h2')
-
-novooParagrafo.innerText = 'Novo paragrafo criado do JS'
-
-body.append (novoParagrafo)
-
-novoParagrafo.style.color ='red'*/
-
-/*
-const nome = document.querySelector('#nome')
-console.log(nome)
-
-const sobrenome = document.querySelector('#sobrenome')
-console.log(sobrenome)
-
-const altura = document.querySelector('#altura')
-console.log(altura)
-
-const peso = document.querySelector('#nome')
-console.log(peso)
-
-const botao = document.querySelector('button')
-const form = document.querySelector('form')
-
-console.log(botao, form)
-
-function teste(){
-    form.addEventListener('submit',()=>{
-        evento.prevenDeFault()
-        window.alert('Evento Recebido')
-    })
-
+  // Com base no valor do dados.imc, faz a verificação da faixa de peso
+  if (dados.imc < 18.5) {
+    console.log(`Paciente abaixo do peso. IMC é de: ${dados.imc.toFixed(2)} `);
+  } else if (dados.imc >= 18.5 && dados.imc < 24.9) {
+    console.log(`Paciente tem peso normal. IMC é de: ${dados.imc.toFixed(2)} `);
+  } else if (dados.imc >= 24.9 && dados.imc < 29.9) {
+    console.log(
+      `Paciente tem excesso de peso. IMC é de: ${dados.imc.toFixed(2)} `
+    );
+  } else if (dados.imc >= 29.9 && dados.imc < 34.9) {
+    console.log(
+      `Paciente tem obesidade classe 1. IMC é de: ${dados.imc.toFixed(2)} `
+    );
+  } else if (dados.imc >= 34.9 && dados.imc < 39.9) {
+    console.log(
+      `Paciente tem obesidade classe 2. IMC é de: ${dados.imc.toFixed(2)} `
+    );
+  } else {
+    console.log(
+      `Paciente tem obesidade classe 3. IMC é de: ${dados.imc.toFixed(2)} `
+    );
+  }
 }
 
-teste()
-*/                              
+// Chamada da função para adicionar / iniciar o monitoramento do evento de submit ao formulário
+capturarDados();
+
+/*  Cálculo do IMC e exibição da categoria correspondente no console.log.
+ Será preciso ajustar o cálculo do IMC para utilizar os valores corretos dos campos
+  de peso e altura que serão digitados no formulário. */
+
+/*
+  if (paciente.imc < 18.5) {
+  console.log(`Paciente abaixo do peso. IMC é de: ${paciente.imc.toFixed(2)} `);
+} else if (paciente.imc >= 18.5 && paciente.imc < 24.9) {
+  console.log(`Paciente tem peso normal. IMC é de: ${paciente.imc.toFixed(2)} `);
+} else if (paciente.imc >= 24.9 && paciente.imc < 29.9) {
+  console.log(`Paciente tem excesso de peso. IMC é de: ${paciente.imc.toFixed(2)} `);
+} else if (paciente.imc >= 29.9 && paciente.imc < 34.9) {
+  console.log(`Paciente tem obesidade classe 1. IMC é de: ${paciente.imc.toFixed(2)} `);
+} else if (paciente.imc >= 34.9 && paciente.imc < 39.9) {
+  console.log(`Paciente tem obesidade classe 2. IMC é de: ${paciente.imc.toFixed(2)} `);
+} else {
+  console.log(`Paciente tem obesidade classe 3. IMC é de: ${paciente.imc.toFixed(2)} `);
+}
+
+
+*/
